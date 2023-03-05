2115dd88aa488309b74f57cfa2b3d992369a625b
jonathan bogado <bogadojonathan@outlook.com>
2023-03-05 20:28:00 -0300

    variables de entorno en setting.py

--- hazvir/settings.py
+++ hazvir/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS")
 
 # Application definition
 APPS_MIAS = [
@@ -89,10 +89,20 @@ WSGI_APPLICATION = 'hazvir.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("NAME_DB"),
+        'USER': config("USER_DB"),
+        'PASSWORD': config("PASSWORD_BD"),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
